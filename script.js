cbce7d2e4ae93314c590e9dcdab8dd23efcb5e57
Bagaski <38439501+bagaski@users.noreply.github.com>
2020-11-14 19:21:55 +0100

    Add files via upload
    Updates: fonts, css media queries, js game over functionality, index.html restructure

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttons = ["btn-stone", "btn-scissors", "btn-paper"]
+
 const randomClasses = ["rock display", "scissors display", "paper display"];
+
 const playerSelect = document.getElementById("player");
 const computerSelect = document.getElementById("computer");
 
@@ -8,6 +10,8 @@ let pScore = 0;
 let cScore = 0;
 const playerScore = document.getElementById("pscore")
 const computerScore = document.getElementById("cscore")
+
+const results = document.getElementById("results")
 
 //play rock:
 
@@ -23,17 +27,13 @@ document.getElementById('btn-rock').addEventListener('click', function() {
     //score.innerHTML = "You win!! :D";
     pScore++ ;
     playerScore.innerHTML = pScore;
-    (cScore < 3 && pScore < 3) ? score.innerHTML="play again" : score.innerHTML=`GAME OVER YOU WON: ${pScore} - ${cScore}
-  `}
+   (cScore < 3 && pScore < 3) ? score.innerHTML="good, play again" : (score.innerHTML=`GAME OVER YOU WON: ${pScore} - ${cScore}`) && (results.style.display = "none")}
 
    else {
      //score.innerHTML = "You lost :O";
      cScore++ ;
      computerScore.innerHTML = cScore;
-     (cScore < 3 && pScore < 3)  ? score.innerHTML="play again" : score.innerHTML=`GAME OVER YOU LOST: ${pScore} - ${cScore}
-  `
-   }
-
+     (cScore < 3 && pScore < 3)  ? score.innerHTML="ahhh, play again" : (score.innerHTML=`GAME OVER YOU LOST: ${pScore} - ${cScore}`) && (results.style.display = "none")}
 });
 
 //play scissors:
@@ -48,17 +48,14 @@ document.getElementById('btn-scissors').addEventListener('click', function() {
   //score.innerHTML = "You win!! :D"
   pScore++ ;
   playerScore.innerHTML = pScore;
-   (cScore < 3 && pScore < 3) ? score.innerHTML="play again" : score.innerHTML=`GAME OVER YOU WON: ${pScore} - ${cScore}
-  `
+   (cScore < 3 && pScore < 3) ? score.innerHTML="yes! play again" : (score.innerHTML=`GAME OVER YOU WON: ${pScore} - ${cScore}`) && (results.style.display = "none");
   }
 
    else {
     //score.innerHTML = "You lost :O";
     cScore++ ;
     computerScore.innerHTML = cScore;
-     (cScore < 3 && pScore < 3)  ? score.innerHTML="play again" : score.innerHTML=`GAME OVER YOU LOST: ${pScore} - ${cScore}
-  `
-   }
+     (cScore < 3 && pScore < 3)  ? score.innerHTML="oh, play again" : (score.innerHTML=`GAME OVER YOU LOST: ${pScore} - ${cScore}`) && (results.style.display = "none")}
 
 });
 
@@ -68,21 +65,19 @@ document.getElementById('btn-paper').addEventListener('click', function() {
   playerSelect.className = "paper display";
   computerSelect.className = randomClasses[Math.floor(Math.random() * randomClasses.length)];
 
-  if (playerSelect.className === computerSelect.className) { score.innerHTML = "draw play again"
+  if (playerSelect.className === computerSelect.className) { score.innerHTML = "Draw! play again"
   }
   else if (computerSelect.className === "rock display") {
-    score.innerHTML = "nice :) play again";
+    score.innerHTML = "play again";
     pScore++ ;
     playerScore.innerHTML = pScore;
-    (cScore < 3 && pScore < 3) ? score.innerHTML="cool play again" : score.innerHTML=`GAME OVER YOU WON: ${pScore} - ${cScore}
-  `
+    (cScore < 3 && pScore < 3) ? score.innerHTML="nice, play again" : (score.innerHTML=`GAME OVER YOU WON: ${pScore} - ${cScore}`) && (results.style.display = "none")
   }
    else {
-     score.innerHTML = "oh play again";
+     score.innerHTML = "play again";
      cScore++ ;
      computerScore.innerHTML = cScore;
-     (cScore < 3 && pScore < 3)  ? score.innerHTML="ahh play again" : score.innerHTML=`GAME OVER YOU LOST: ${pScore} - ${cScore}
-  `
+     (cScore < 3 && pScore < 3) ? score.innerHTML="bad, play again" : (score.innerHTML=`GAME OVER YOU LOST: ${pScore} - ${cScore}`) && (results.style.display = "none")
    }
 
 });
